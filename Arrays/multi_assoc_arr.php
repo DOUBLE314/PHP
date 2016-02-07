8e6e647e7b046ae8580128cbe422193caf884fa1
Valerych <valerij.zhigunov@gmail.com>
2016-02-07 18:28:01 +0300

    Разные массивы

--- Arrays/multi_assoc_arr.php
+++ Arrays/multi_assoc_arr.php
@@ -7,5 +7,6 @@
 $emp["Ivanov"] = array("name"=>"Иванов И.И.", "age"=>"25", "email"=>"ivanov@mail.ru");
 $emp["Petrov"] = array("name"=>"Петров П.П.", "age"=>"34", "email"=>"petrov@mail.ru");
 $emp["Sidorov"] = array("name"=>"Сидоров С.С.", "age"=>"47", "email"=>"sidorov@mail.ru");
+echo $emp;
 
 ?>
