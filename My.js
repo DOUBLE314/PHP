4b198823087b004e083c7c8d978e1fa5ab1bdecc
Valerych <valerij.zhigunov@gmail.com>
2016-02-07 18:31:10 +0300

    Разные массивы

---
+++ My.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Валерий on 20.01.2016.
+ */
+function factorial(n) {
+var product =1;
+    while (n>1) {
+        product*=n;
+        n--;
+        
+    }
+
+}
---
+++ first.php
@@ -0,0 +1,25 @@
+<?php
+$var ='Bob';
+$Var='Jack';
+    $x=25;
+    $y=30.54;
+    $z='Hello World';
+    $f='true';
+    echo "$x\n";
+    echo "$y\n";
+    echo "$z\n";
+    echo "$f\n";
+    echo "$var, $Var";
+?>
+<?php $h='Hello';
+$w='World';
+$con=$h.$w;
+echo $con;
+?>
+<?php
+echo"Приветствую вас здесь! Слышал, что вы учитесь на PHP-программиста!\n";
+echo"Наберите свое имя для меня:\n";
+$name=trim(fgets(STDIN));
+echo "\nСпасибо, ".$name.", очень рад с вами познакомиться. \n\n";
+?>
+
